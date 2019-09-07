f24b27a4b4f2fbe72748758929909644f0c9a4d0
Ilya02k <53654153+Ilya02k@users.noreply.github.com>
2019-09-07 13:04:50 +0300

    Add files via upload

---
+++ 1.6TaskAboutCycle.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	for(int i=0; i<256;i++)
+    {
+        char b=(char)i;
+        System.out.println(i+"   "+b);
+    }
+    }
+}
