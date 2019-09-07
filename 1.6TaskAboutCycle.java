9f75cf1666446aa6b1a40266e9750f27dff87da1
Ilya02k <53654153+Ilya02k@users.noreply.github.com>
2019-09-07 13:03:10 +0300

    Add files via upload

--- 1.6TaskAboutCycle.java
+++ 1.6TaskAboutCycle.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	for(int i=0; i<256;i++)
-    {
-        char b=(char)i;
-        System.out.println(i+"   "+b);
-    }
+        int m, n;
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Input m and n: ");
+        m = in.nextInt();
+        n = in.nextInt();
+        while (m <= n) {
+            System.out.print(m + ": ");
+            for (int i = 2; i < m - 1; i++) {
+                if (m % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+            m += 1;
+        }
+
     }
 }
